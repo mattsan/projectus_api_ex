584130bd9a8b3941bc7ae6a9dcc1f4d9c0fe2d6b
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2018-04-09 09:47:56 +0900

    add a seeding script

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,22 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias ProjectusApi.Repo
+alias ProjectusApi.Missions.Task
+
+timestamp = NaiveDateTime.utc_now
+
+entries =
+  1..100
+  |> Enum.map(fn _ ->
+      %{
+        title: Faker.Lorem.sentence(),
+        description: Faker.Lorem.sentence(),
+        status: :rand.uniform(3) - 1,
+        inserted_at: timestamp,
+        updated_at: timestamp
+      }
+    end)
+
+Repo.insert_all(Task, entries)
