f20aff9368d828c3a2ed0b3291b2fae29cbaa963
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2018-04-12 11:17:39 +0900

    add conversions of status

--- lib/projectus_api/missions/task.ex
+++ lib/projectus_api/missions/task.ex
@@ -2,7 +2,6 @@ defmodule ProjectusApi.Missions.Task do
   use Ecto.Schema
   import Ecto.Changeset
 
-
   schema "tasks" do
     field :description, :string
     field :title, :string
@@ -14,7 +13,48 @@ defmodule ProjectusApi.Missions.Task do
   @doc false
   def changeset(task, attrs) do
     task
-    |> cast(attrs, [:title, :description, :status])
+    |> cast(status_to_number(attrs), [:title, :description, :status])
     |> validate_required([:title, :description])
+    |> validate_number(:status, greater_than_or_equal_to: 0, less_than_or_equal_to: 2)
+  end
+
+  def status_to_string(%{status: status} = task)
+    when is_integer(status) or is_nil(status),
+    do: %{task | status: conviert_status_to_string(status)}
+
+  def status_to_string(%{"status" => status} = task)
+    when is_integer(status) or is_nil(status),
+    do: %{task | "status" => conviert_status_to_string(status)}
+
+  def status_to_string(%{} = task), do: task
+
+  def status_to_number(%{status: status} = task)
+    when is_binary(status) or is_nil(status),
+    do: %{task | status: conviert_status_to_number(status)}
+
+  def status_to_number(%{"status" => status} = task)
+    when is_binary(status) or is_nil(status),
+    do: %{task | "status" => conviert_status_to_number(status)}
+
+  def status_to_number(%{} = task), do: task
+
+  defp conviert_status_to_string(status) do
+    case status do
+      nil -> "todo"
+      0 -> "todo"
+      1 -> "doing"
+      2 -> "done"
+      _ -> status
+    end
+  end
+
+  defp conviert_status_to_number(status) do
+    case status do
+      nil -> 0
+      "todo" -> 0
+      "doing" -> 1
+      "done" -> 2
+      _ -> status
+    end
   end
 end
--- lib/projectus_api_web/views/task_view.ex
+++ lib/projectus_api_web/views/task_view.ex
@@ -1,6 +1,7 @@
 defmodule ProjectusApiWeb.TaskView do
   use ProjectusApiWeb, :view
   alias ProjectusApiWeb.TaskView
+  alias ProjectusApi.Missions.Task
 
   def render("index.json", %{tasks: tasks}) do
     render_many(tasks, TaskView, "task.json")
@@ -11,13 +12,11 @@ defmodule ProjectusApiWeb.TaskView do
   end
 
   def render("task.json", %{task: task}) do
-    %{id: task.id,
+    %{
+      id: task.id,
       title: task.title,
       description: task.description,
-      status: status_to_string(task.status)}
+      status: Task.status_to_string(task).status
+    }
   end
-
-  def status_to_string(0), do: "todo"
-  def status_to_string(1), do: "doing"
-  def status_to_string(2), do: "done"
 end
